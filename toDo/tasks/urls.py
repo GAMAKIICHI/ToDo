a0e42a285b689d76c94023aa51098e7dbb02537a
Emanuel <efabio05@icloud.com>
2024-09-13 12:49:57 -0700

    setup views

--- toDo/tasks/urls.py
+++ toDo/tasks/urls.py
@@ -1,6 +1,10 @@
 from django.urls import path
 from . import views
 
+app_name = "tasks"
 urlpatterns = [
+    #ex: /tasks/
     path("", views.index, name="index"),
+    #ex: /tasks/1/
+    path("<int:task_id>/", views.detail, name="detail"),
 ]
