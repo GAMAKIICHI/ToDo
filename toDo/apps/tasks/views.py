5efcfc9d22f35ac576e75c56acb71fdfabc89a61
Emanuel <efabio05@icloud.com>
2024-11-22 13:00:19 -0800

    created completed task page

--- toDo/apps/tasks/views.py
+++ toDo/apps/tasks/views.py
@@ -1,62 +1,117 @@
-from django.shortcuts import render
+from django.core.exceptions import ValidationError, ObjectDoesNotExist
+from django.shortcuts import render, redirect
 from django.contrib.auth.decorators import login_required
-from apps.tasks.models import TaskForm
-from apps.tasks.models import TaskModel
-import logging.config
+from apps.tasks.models import TaskForm, TaskModel
 from datetime import datetime
-from django.http import HttpResponseRedirect
-from django.contrib import messages
-from django.shortcuts import get_object_or_404
+from django import forms
+import logging
 
-logger = logging.getLogger("DEBUG")
+logger = logging.getLogger()
 
 @login_required(login_url='/accounts/login')
 def index(request):
 
-    current_user = request.user
-
+    currentUser = request.user
     taskForm = None
 
     if request.method == "POST":
-        taskForm = TaskForm(request.POST)
+        if "create_task" in request.POST:
+            taskForm = TaskForm(request.POST) 
+            if createTask(currentUser, taskForm):
+                #needs to be redirected or form will try to resumbit when page is refreshed
+                return redirect(request.path_info)
 
-        if taskForm.is_valid():
-            #allows form to be modified before saving
-            taskForm = taskForm.save(commit=False)
-            taskForm.user = current_user
+        if "delete_task" in request.POST:
+            taskId = request.POST["delete_task"]
+            if deleteTask(taskId):
+                return redirect(request.path_info)
 
-            #this autofills date/time to current date/time if left blank
-            if not taskForm.deadline:
-                taskForm.deadline = datetime.now().strftime("%Y-%m-%dT%H:%M:%SZ")
-               
-            taskForm.save()
-
-            #needs to be redirected or form will try to resumbit by default on some browsers
-            return HttpResponseRedirect("/tasks/dashboard/")
-    else:
-        taskForm = TaskForm()
+        if "complete_task" in request.POST:
+            taskId = request.POST["complete_task"]
+            if updateTaskStatus(taskId):
+                return redirect(request.path_info)
     
-    if request.method == "GET":
-        completion_id = request.GET.get('complete-task')
-        delete_id = request.GET.get('delete-task')
+    tasks = TaskModel.objects.filter(user=currentUser, completion_status=False)
 
-        if completion_id != None:
-            task_instance = get_object_or_404(TaskModel, id=completion_id)
-            task_instance.completion_status = not task_instance.completion_status
-            task_instance.save()
-        
-        if delete_id != None:
-            task_instance = get_object_or_404(TaskModel, id=delete_id)
-            task_instance.delete()
-            return HttpResponseRedirect("/tasks/dashboard/")
-            
+    return render(request, "tasks/dashboard.html", {"tasks": tasks, "formErrors": getFormError(taskForm)})
 
-    tasks = TaskModel.objects.filter(user=current_user)
+@login_required(login_url='/accounts/login')
+def completed(request):
 
-    return render(request, "tasks/dashboard.html", {"tasks": tasks, "form": taskForm})
+    current_user = request.user
+
+    tasks = TaskModel.objects.filter(user=current_user, completion_status=True)
+
+    if request.method == "POST":
+        if "delete_task" in request.POST:
+            taskId = request.POST["delete_task"]
+            if deleteTask(taskId):
+                return redirect(request.path_info)
+
+        if "complete_task" in request.POST:
+            taskId = request.POST["complete_task"]
+            if updateTaskStatus(taskId):
+                return redirect(request.path_info)
+
+    return render(request, "tasks/complete.html", {"tasks": tasks})
 
 def detail(request, task_id):
 
-    # task = get_object_or_404(TaskForm, pk=task_id)
-
     return render(request, "tasks/detail.html")
+
+def createTask(currentUser, taskForm:forms.ModelForm):
+    try:
+        if not taskForm.is_valid():
+            raise ValidationError(f"{getError(taskForm.errors)[0]}")
+        
+        #allows form to be modified before saving
+        taskForm = taskForm.save(commit=False)
+        taskForm.user = currentUser
+
+        #this autofills date/time to current date/time if left blank
+        if not taskForm.deadline:
+            taskForm.deadline = datetime.now().strftime("%Y-%m-%dT%H:%M:%SZ")
+        
+        taskForm.save()
+
+        logger.info(f"{currentUser.id} created task #{taskForm.id}")
+        return True
+
+    except ValidationError as err:
+        logger.error(f"Form Validation: {err}")
+
+#updates task completion status by task id
+def updateTaskStatus(taskId:int):
+    try:
+        task_instance = TaskModel.objects.get(id=taskId)
+        task_instance.completion_status = not task_instance.completion_status
+        task_instance.save()
+        logger.info(f"{taskId} has beed updated.")
+        return True
+    except ObjectDoesNotExist as err:
+        logger.error(f"Updating Task: {err}")
+
+#deletes task user selected by task id
+def deleteTask(taskId:int):
+    try:
+        task_instance = TaskModel.objects.get(id=taskId)
+        task_instance.delete()
+        logger.info(f"{taskId} has beed deleted.")
+        return True
+    except ObjectDoesNotExist as err:
+        logger.error(f"Deleting Task: {err}")
+
+#returns a formated list with all of the errors
+def getError(errorDict:dict):
+    try:
+        errorList = [error for error_list in errorDict.values() for error in error_list]
+        return errorList
+    except Exception as err:
+        logger.error(f"{err}")
+
+def getFormError(taskForm:forms.ModelForm):
+    try:
+        if not taskForm.is_valid():
+            return taskForm.errors
+    except:
+        logger.info(f"No Form Errors are Available.")
